c44f0653ed3b323ec0f67ea414a232c7cca3c4f4
Matsuki.Hidenori <info@mazgi.info>
2012-05-20 17:52:28 +0900

    create project

---
+++ Prj20120520/Prj20120520/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Prj20120520
+//
+//  Created by Hidenori Matsuki on 12/05/20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
