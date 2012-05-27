4bb16db545299b08e79ce773b7f4319d87450176
Matsuki.Hidenori <info@mazgi.info>
2012-05-27 17:51:59 +0900

    ARCの動作確認用クラスを作成

---
+++ Prj20120527/Prj20120527/ARCTestObject.h
@@ -0,0 +1,13 @@
+//
+//  ARCTestObject.h
+//  Prj20120527
+//
+//  Created by Hidenori Matsuki on 12/05/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARCTestObject : NSObject
+
+@end
---
+++ Prj20120527/Prj20120527/ARCTestObject.m
@@ -0,0 +1,13 @@
+//
+//  ARCTestObject.m
+//  Prj20120527
+//
+//  Created by Hidenori Matsuki on 12/05/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ARCTestObject.h"
+
+@implementation ARCTestObject
+
+@end
